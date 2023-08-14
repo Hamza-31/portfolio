7f2ee0c8615cf3d717d13edf8308b75ff232c356
Hamza <hamza.labzioui@gmail.com>
2023-08-15 01:58:05 +0200

    Remove header tags from project component

--- components/SinglePost.tsx
+++ components/SinglePost.tsx
@@ -7,7 +7,7 @@ const SinglePost = ({ post }: any) => {
 		<li className='py-7 mx-3'>
 			<article className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
 				<div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
-				<header className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2">{post.date}</header>
+				<div className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2">{post.date}</div>
 				<div className="z-10 sm:col-span-6 col-span-5">
 					<h3>
 						<div className="inline-flex items-baseline font-medium leading-tight text-slate-500 max-[1024px]:text-teal-300 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base">
